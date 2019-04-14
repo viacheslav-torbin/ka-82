f58776fb6c9b9670dca4436c7578132374b0ee61
torbinofficial <torbin.official@gmail.com>
2019-04-14 12:48:23 +0300

    Minor updates

--- task4-16.cpp
+++ task4-16.cpp
@@ -19,10 +19,12 @@ public:
   void setPayment(int pay) { payment = pay; };
   void print();
   Poem operator+(Poem);
-  friend ostream & operator << (ostream & out, Poem & p){
-    out << "Title: " << p.title << "\n" <<"Size: " << p.size << "\n" << "Payment: " << p.payment << endl;
+  friend ostream &operator<<(ostream &out, Poem &p) {
+    out << "Title: " << p.title << "\n"
+        << "Size: " << p.size << "\n"
+        << "Payment: " << p.payment << endl;
   }
-  friend istream & operator >> (istream & in, Poem & p){
+  friend istream &operator>>(istream &in, Poem &p) {
     in >> p.title >> p.size >> p.payment;
   }
   ~Poem();
@@ -41,17 +43,18 @@ public:
   char *getName() { return name; };
   char *getSurname() { return surname; };
   void print();
-  bool operator==(Autor &a){
-    return ((strcmp(this->name, a.name) == 0) && (strcmp(this->surname, a.surname) == 0));
+  bool operator==(Autor &a) {
+    return ((strcmp(this->name, a.name) == 0) &&
+            (strcmp(this->surname, a.surname) == 0));
   }
   ~Autor();
-  friend ostream & operator << (ostream & out, Autor & a){
-    return out << "Name: " << a.name << "\n" <<"Surname: " << a.surname << endl;
+  friend ostream &operator<<(ostream &out, Autor &a) {
+    return out << "Name: " << a.name << "\n"
+               << "Surname: " << a.surname << endl;
   }
-  friend istream & operator >> (istream & in, Autor & a){
+  friend istream &operator>>(istream &in, Autor &a) {
     return in >> a.name >> a.surname;
   }
-
 };
 
 class Music : public Poem {
@@ -60,14 +63,12 @@ private:
   Autor autor;
 
 public:
-  Music():Poem(), duration(-1.0), autor(){
-  };
+  Music() : Poem(), duration(-1.0), autor(){};
   Music(char *t, int s, int p, float d, Autor a)
-      : Poem(t, s, p), duration(d), autor(a){
-      };
+      : Poem(t, s, p), duration(d), autor(a){};
   void setDuration(float d) { duration = d; };
   float getDuration() { return duration; };
-  void print(){
+  void print() {
     Poem::print();
     cout << "Duration:" << duration << endl;
     autor.print();
@@ -80,26 +81,22 @@ private:
   Autor autor;
 
 public:
-  Book():Poem(), pages(-1), autor(){
-  };
+  Book() : Poem(), pages(-1), autor(){};
   Book(char *t, int s, int pay, int pages, Autor a)
-      : Poem(t, s, pay), pages(pages), autor(a){
-    };
+      : Poem(t, s, pay), pages(pages), autor(a){};
   ~Book(){};
-  void setPages(int p){pages = p;};
-  int getPages(){return pages;};
-  Book& operator++(int i){
+  void setPages(int p) { pages = p; };
+  int getPages() { return pages; };
+  Book &operator++(int i) {
     this->pages++;
     return *this;
   }
-  Book& operator++(){
+  Book &operator++() {
     this->pages = ++pages;
     return *this;
   }
-  bool operator<(Book b){
-    return (this->pages < b.pages);
-  }
-  void print(){
+  bool operator<(Book b) { return (this->pages < b.pages); }
+  void print() {
     Poem::print();
     cout << "Pages:" << pages << endl;
     autor.print();
@@ -114,7 +111,7 @@ int main() {
   cout << "p2" << endl;
   p2.print();
   Poem p3;
-  p3=p1+p2;
+  p3 = p1 + p2;
   cout << "p3" << endl;
   p3.print();
   Book b1;
@@ -160,9 +157,7 @@ void Poem::setTitle(char *title_) {
   strcpy(title, title_);
 };
 
-Poem::~Poem() {
-  delete[] title;
-}
+Poem::~Poem() { delete[] title; }
 
 void Autor::setName(char *n) {
   name = new char[strlen(n)];
@@ -180,13 +175,12 @@ Autor::Autor(char *n, char *s) {
   surname = new char[strlen(n) + 1];
   strcpy(surname, s);
 };
-
-void Autor::print(){
+void Autor::print() {
   cout << "Name:" << name << endl;
   cout << "Surname:" << surname << endl;
 }
 
-void Poem::print(){
+void Poem::print() {
   cout << "Title:" << title << endl;
   cout << "Size:" << size << endl;
   cout << "Payment:" << payment << endl;
@@ -199,11 +193,9 @@ Autor::Autor() {
   strcpy(surname, "None");
 }
 
-Autor::~Autor() {
-  delete[] name, surname;
-}
+Autor::~Autor() { delete[] name, surname; }
 
-Poem Poem::operator+(Poem p){
+Poem Poem::operator+(Poem p) {
   Poem temp;
   temp.payment = this->payment + p.payment;
   return temp;
